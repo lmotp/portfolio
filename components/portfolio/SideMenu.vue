6c79bc8c30a36d42c22b8ebef7a31038bb8d9cc1
lmotp <lmotp@daum.net>
2025-02-20 00:13:08 +0900

    feat : nuxt Image 설정

--- components/portfolio/SideMenu.vue
+++ components/portfolio/SideMenu.vue
@@ -14,15 +14,13 @@ const handleMenuToggle = () => {
 
 <template>
   <aside id="side" class="side">
-    <Transition name="toggle">
-      <div v-show="isToggle" class="side__menu">
-        <div v-for="card of cards" :key="`card-${card.key}`">
-          <NuxtImg :src="card.imgSrc" alt="" />
-        </div>
+    <div :class="['side__menu', isToggle && 'is-open']">
+      <div v-for="card of cards" :key="`card-${card.key}`" class="card">
+        <NuxtImg :src="card.imgSrc" alt="" />
       </div>
-    </Transition>
 
-    <button class="side__toggle-btn" @click="handleMenuToggle">{{ isToggle ? "열기" : "닫기" }}</button>
+      <button class="side__toggle-btn" @click="handleMenuToggle">{{ isToggle ? "닫기" : "열기" }}</button>
+    </div>
   </aside>
 </template>
 
@@ -33,19 +31,32 @@ const handleMenuToggle = () => {
   isolation: isolate;
 
   .side__menu {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 8px;
     width: 60dvw;
     height: 100%;
     background-color: white;
     z-index: -1;
+    transform: translateX(-100%);
+    transition: transform 750ms cubic-bezier(0.08, 0.82, 0.17, 1);
 
-    &.toggle-enter-active,
-    &.toggle-leave-active {
-      transition: transform 750ms cubic-bezier(0.08, 0.82, 0.17, 1);
+    &.is-open {
+      transform: translateX(0);
     }
 
-    &.toggle-enter-from,
-    &.toggle-leave-to {
-      transform: translate(-100%);
+    .card {
+      width: 100%;
+      aspect-ratio: 0.6;
+      border-radius: 20px;
+      outline: 1px solid red;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
     }
   }
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["@/assets/css/main.css"],
 
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxt/image"],
 
   build: {
     transpile: ["three"],
