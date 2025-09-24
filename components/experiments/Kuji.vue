13c3933ccbeebda8008eb76ecec525cc26e63d6c
lmotp <lmotp@daum.net>
2025-09-24 14:54:19 +0900

    feat : add shadwo

--- components/experiments/Kuji.vue
+++ components/experiments/Kuji.vue
@@ -3,12 +3,7 @@ import * as THREE from "three";
 import * as dat from "lil-gui";
 import { SVGLoader } from "three/examples/jsm/Addons.js";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
-
-import ImageVertex from "~/shaders/kuji/imageVertex.glsl";
-import ImageFragment from "~/shaders/kuji/imageFragment.glsl";
-
-import KujiVertex from "~/shaders/kuji/kujiVertex.glsl";
-import KujiFragment from "~/shaders/kuji/kujiFragment.glsl";
+import { TransformControls } from "three/examples/jsm/Addons.js";
 
 const kujiRef = ref<HTMLElement | null>(null);
 const resolution = ref(new THREE.Vector2(window.innerWidth, window.innerHeight));
@@ -16,15 +11,25 @@ const resolution = ref(new THREE.Vector2(window.innerWidth, window.innerHeight))
 let renderer: THREE.WebGLRenderer;
 let scene: THREE.Scene;
 let camera: THREE.PerspectiveCamera;
+
+let ambientLight: THREE.AmbientLight;
+let dirLight: THREE.DirectionalLight;
+
 let controls: OrbitControls;
+let transformControls: TransformControls;
 
 let geometry: THREE.PlaneGeometry;
-let material: THREE.ShaderMaterial;
+let material: THREE.MeshStandardMaterial;
 let mesh: THREE.Mesh;
+
+let svgGeometry: THREE.ShapeGeometry;
+let svgMaterial: THREE.MeshStandardMaterial;
+let svgMesh: THREE.Mesh;
 
 const gui = new dat.GUI();
 const settings = ref({
-  scale: 0.1,
+  scale: 0.0085,
+  intensity: 1.5,
 });
 
 const init = () => {
@@ -32,36 +37,50 @@ const init = () => {
 
   renderer = new THREE.WebGLRenderer({ logarithmicDepthBuffer: true, canvas: kujiRef.value });
   renderer.setSize(resolution.value.x, resolution.value.y);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setClearColor(0x0e0e0e, 1.0);
+  renderer.setClearColor(0xffffff, 1.0);
 
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(45, resolution.value.x / resolution.value.y, 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(50, resolution.value.x / resolution.value.y, 0.1, 1000);
   camera.position.set(0, 0, 150);
 
   controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
 
+  transformControls = new TransformControls(camera, renderer.domElement);
+  transformControls.addEventListener("change", () => {
+    renderer.render(scene, camera);
+  });
+  transformControls.addEventListener("dragging-changed", (event) => {
+    controls.enabled = !event.value;
+  });
+
+  const gizmo = transformControls.getHelper();
+  scene.add(gizmo);
+
   setPlaneMesh();
   setSVGMesh();
+
+  setLights();
 };
 
 const setPlaneMesh = () => {
   const textureLoader = new THREE.TextureLoader();
-  const imageTex = textureLoader.load("/images/experiments/kuji/kuji_front.jpg");
+  textureLoader.load("/images/experiments/kuji/kuji_front.jpg", (tex) => {
+    tex.wrapS = THREE.RepeatWrapping;
+    tex.wrapT = THREE.RepeatWrapping;
 
-  geometry = new THREE.PlaneGeometry(120, 80);
-  material = new THREE.ShaderMaterial({
-    uniforms: {
-      imgRatio: { value: new THREE.Vector2() },
-      uTexture: { value: imageTex },
-    },
-    vertexShader: ImageVertex,
-    fragmentShader: ImageFragment,
-    side: THREE.DoubleSide,
+    geometry = new THREE.PlaneGeometry(120, 80);
+    material = new THREE.MeshStandardMaterial({
+      map: tex,
+      side: THREE.DoubleSide,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.receiveShadow = true;
+    scene.add(mesh);
   });
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
 };
 
 const setSVGMesh = () => {
@@ -69,10 +88,10 @@ const setSVGMesh = () => {
   svgLoader.load("/images/experiments/kuji/kuji_point.svg", (data) => {
     const path = data.paths[0];
     const shapes = SVGLoader.createShapes(path);
-    const scaleSize = 0.09;
+    const scaleSize = 0.086;
 
-    const geometry = new THREE.ShapeGeometry(shapes[0]);
-    const positions = geometry.attributes.position.array;
+    svgGeometry = new THREE.ShapeGeometry(shapes[0]);
+    const positions = svgGeometry.attributes.position.array;
     const uvs = new Float32Array((positions.length * 2) / 3);
 
     let minX = Infinity;
@@ -97,31 +116,61 @@ const setSVGMesh = () => {
       uvs[(k / 3) * 2 + 1] = (y - minY) / height;
     }
 
-    geometry.setAttribute("uv", new THREE.BufferAttribute(uvs, 2));
+    svgGeometry.setAttribute("uv", new THREE.BufferAttribute(uvs, 2));
 
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        uTexture: { value: null },
-      },
-      vertexShader: KujiVertex,
-      fragmentShader: KujiFragment,
+    svgMaterial = new THREE.MeshStandardMaterial({
+      color: 0xff0000,
       side: THREE.DoubleSide,
+
+      // opacity: 0.5,
+      // transparent: true,
     });
+    svgMesh = new THREE.Mesh(svgGeometry, svgMaterial);
+    svgMesh.castShadow = true;
+    svgMesh.scale.y *= -1;
+    svgMesh.scale.set(scaleSize, scaleSize, scaleSize);
 
-    const mesh = new THREE.Mesh(geometry, material);
-
-    mesh.scale.y *= -1;
-    mesh.scale.set(scaleSize, scaleSize, scaleSize);
-
-    scene.add(mesh);
-
-    const bbox = new THREE.Box3().setFromObject(mesh);
+    const bbox = new THREE.Box3().setFromObject(svgMesh);
     const center = bbox.getCenter(new THREE.Vector3());
 
-    mesh.position.x -= center.x;
-    mesh.position.y -= center.y;
-    mesh.position.z = 0.1;
+    svgMesh.position.x -= center.x - 0.5;
+    svgMesh.position.y -= center.y - 0.5;
+    svgMesh.position.z = 0.1;
+
+    svgMesh.rotateY(THREE.MathUtils.degToRad(-30));
+
+    scene.add(svgMesh);
   });
+};
+
+const setLights = () => {
+  dirLight = new THREE.DirectionalLight(0xffffff, 3.15);
+
+  dirLight.castShadow = true;
+  // ✨ 그림자 카메라의 범위와 해상도 조절 (매우 중요)
+  dirLight.shadow.mapSize.width = 2048; // 해상도 높이기
+  dirLight.shadow.mapSize.height = 2048;
+  dirLight.shadow.camera.left = -60;
+  dirLight.shadow.camera.right = 60;
+  dirLight.shadow.camera.top = 40;
+  dirLight.shadow.camera.bottom = -40;
+  dirLight.shadow.camera.near = 100;
+  dirLight.shadow.camera.far = 200;
+
+  dirLight.position.set(0, 0, 150);
+  scene.add(dirLight);
+
+  // const rectLight = new THREE.RectAreaLight(0xffffff, 1, 1, 1);
+  // rectLight.position.set(0, 0, 150);
+  // scene.add(rectLight);
+
+  // const pointLight = new THREE.PointLight(0x0000ff, 1);
+  // const pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
+  // pointLight.position.set(0, 0, 10);
+  // scene.add(pointLight);
+  // scene.add(pointLightHelper);
+
+  // transformControls.attach(pointLight);
 };
 
 const resizeWindow = () => {
--- shaders/kuji/kujiVertex.glsl
+++ shaders/kuji/kujiVertex.glsl
@@ -1,6 +1,17 @@
 varying vec2 vUv;
 
-void main(void) {
+float plot(vec2 st, float pct) {
+  return smoothstep(pct - 0.02, pct, st.y) -
+    smoothstep(pct, pct + 0.02, st.y);
+}
+
+void main() {
   vUv = uv;
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  float reverseUv = 1.0 - uv.x;
+
+  modelPosition.z += reverseUv * 10.0;
+
+  gl_Position = projectionMatrix * viewMatrix * modelPosition;
 }
