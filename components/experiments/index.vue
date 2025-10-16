cf7a42110124726ecfff635587e1b4ecee71ea32
lmotp <lmotp@daum.net>
2025-10-16 16:56:59 +0900

    feat : experiments animation

--- components/experiments/index.vue
+++ components/experiments/index.vue
@@ -6,26 +6,32 @@ const menuList = [
   {
     name: "Burn",
     path: "/experiments/burn",
+    src: "/images/skills/0.webp",
   },
   {
     name: "Glitch",
     path: "/experiments/glitch",
+    src: "/images/skills/1.webp",
   },
   {
     name: "Kuji",
     path: "/experiments/kuji",
+    src: "/images/skills/2.webp",
   },
   {
     name: "Roll",
     path: "/experiments/roll",
+    src: "/images/skills/3.webp",
   },
   {
     name: "Pinball",
     path: "/experiments/pinball",
+    src: "/images/skills/4.webp",
   },
   {
     name: "Blur",
     path: "/experiments/blur",
+    src: "/images/skills/5.webp",
   },
 ];
 
@@ -40,11 +46,18 @@ const handleMenuClick = (menuPath: string) => {
 </script>
 
 <template>
-  <div>
-    메인입니다
-    <div class="link-wrap">
-      <button v-for="menu in menuList" :key="menu.path" @click="handleMenuClick(menu.path)">
-        {{ menu.name }}
+  <div class="link-wrap">
+    <div class="button-wrap">
+      <button
+        v-for="menu in menuList"
+        :key="menu.path"
+        @click="handleMenuClick(menu.path)"
+        :style="{ '--data-src': `url(${menu.src})` }"
+      >
+        <div class="text-wrap">
+          <div class="blend"></div>
+          <strong>{{ menu.name }}</strong>
+        </div>
       </button>
     </div>
   </div>
@@ -55,6 +68,53 @@ const handleMenuClick = (menuPath: string) => {
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 20px;
+  width: 100%;
+  height: 100dvh;
+
+  .button-wrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    width: 100%;
+    max-width: 1000px;
+
+    button {
+      display: flex;
+      align-items: flex-end;
+      flex: 1;
+      height: 300px;
+      border-radius: 12px;
+      background: var(--data-src) center;
+      background-size: cover;
+      overflow: hidden;
+
+      transition: flex 0.3s ease-in-out;
+
+      &:hover {
+        flex: 5;
+      }
+
+      .text-wrap {
+        position: relative;
+        width: 100%;
+        text-align: left;
+
+        .blend {
+          position: absolute;
+          inset: 0;
+          background-color: red;
+          mix-blend-mode: multiply;
+        }
+
+        strong {
+          display: inline-block;
+          padding: 8px;
+          font-size: 16px;
+          color: white;
+        }
+      }
+    }
+  }
 }
 </style>
