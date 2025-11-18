22fcbd36b73f31ef12eb24b660b5eac9e91deb54
lmotp <lmotp@daum.net>
2025-11-18 15:29:43 +0900

    add : brush experiments

---
+++ components/experiments/Brush.vue
@@ -0,0 +1,166 @@
+<script setup lang="ts">
+import * as THREE from "three";
+
+import vertexShader from "@/shaders/brush/vertex.glsl";
+import fragmentShader from "@/shaders/brush/fragment.glsl";
+
+const brushRef = ref<HTMLElement | null>(null);
+const mouse = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight;
+let time = 0;
+
+let renderer: THREE.WebGLRenderer;
+let camera: THREE.PerspectiveCamera;
+let scene: THREE.Scene;
+
+let whiteTarget: THREE.WebGLRenderTarget;
+let whiteScene: THREE.Scene;
+let whiteBg: THREE.Mesh;
+let box: THREE.Mesh;
+
+let geometry: THREE.PlaneGeometry;
+let material: THREE.MeshBasicMaterial;
+let mesh: THREE.Mesh;
+
+let mouseGeometry: THREE.PlaneGeometry | THREE.SphereGeometry;
+let mouseMaterial: THREE.MeshBasicMaterial;
+let mouseMesh: THREE.Mesh;
+
+let targetA: THREE.WebGLRenderTarget;
+let targetB: THREE.WebGLRenderTarget;
+
+let sourceTarget: THREE.WebGLRenderTarget;
+let fboScene: THREE.Scene;
+let fboCamera: THREE.OrthographicCamera;
+let fboMaterial: THREE.ShaderMaterial;
+let fboGeometry: THREE.PlaneGeometry;
+let fboQuad: THREE.Mesh;
+
+let finalScene: THREE.Scene;
+let finalQuad: THREE.Mesh;
+
+const init = () => {
+  if (!brushRef.value) return;
+
+  renderer = new THREE.WebGLRenderer({
+    canvas: brushRef.value,
+    antialias: true,
+    alpha: false,
+  });
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setSize(canvasWidth, canvasHeight);
+
+  camera = new THREE.PerspectiveCamera(70, canvasWidth / canvasHeight, 0.1, 1000);
+  camera.position.set(0, 0, 2);
+
+  scene = new THREE.Scene();
+
+  whiteTarget = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight);
+  whiteScene = new THREE.Scene();
+  whiteBg = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({ color: 0xffffff }));
+  whiteBg.position.set(0, 0, -1);
+  whiteScene.add(whiteBg);
+
+  box = new THREE.Mesh(new THREE.BoxGeometry(0.3, 0.3, 0.3), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+  whiteScene.add(box);
+
+  setupPipeline();
+  mouseEvents();
+
+  animate();
+};
+
+const setupPipeline = () => {
+  sourceTarget = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight);
+
+  targetA = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight);
+  targetB = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight);
+
+  renderer.setRenderTarget(whiteTarget);
+  renderer.render(whiteScene, camera);
+
+  fboScene = new THREE.Scene();
+  fboCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+  fboGeometry = new THREE.PlaneGeometry(2, 2);
+  fboMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      tDiffuse: { value: null },
+      tPrev: { value: whiteTarget.texture },
+      uTime: { value: 0 },
+      uResolution: { value: new THREE.Vector4(canvasWidth, canvasHeight, 1, 1) },
+    },
+    vertexShader,
+    fragmentShader,
+  });
+  fboQuad = new THREE.Mesh(fboGeometry, fboMaterial);
+  fboScene.add(fboQuad);
+
+  finalScene = new THREE.Scene();
+  finalQuad = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), new THREE.MeshBasicMaterial({ map: targetA.texture }));
+  finalScene.add(finalQuad);
+};
+
+const mouseEvents = () => {
+  geometry = new THREE.PlaneGeometry(100, 100, 32, 32);
+  material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+  mesh = new THREE.Mesh(geometry, material);
+
+  // mouseGeometry = new THREE.PlaneGeometry(0.5, 0.5, 20, 20);
+  mouseGeometry = new THREE.SphereGeometry(0.2, 20, 20);
+  mouseMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    // transparent: true,
+    // map: new THREE.TextureLoader().load(usePublicAsset("/images/experiments/brush/ball.png")),
+  });
+  mouseMesh = new THREE.Mesh(mouseGeometry, mouseMaterial);
+  scene.add(mouseMesh);
+
+  window.addEventListener("mousemove", (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObject(mesh);
+
+    if (intersects.length > 0) {
+      mouseMesh.position.copy(intersects[0].point);
+    }
+  });
+};
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  time += 0.05;
+
+  renderer.setRenderTarget(sourceTarget);
+  renderer.render(scene, camera);
+
+  renderer.setRenderTarget(targetA);
+  renderer.render(fboScene, fboCamera);
+  fboMaterial.uniforms.tDiffuse.value = sourceTarget.texture;
+  fboMaterial.uniforms.tPrev.value = targetA.texture;
+  fboMaterial.uniforms.uTime.value = time;
+
+  finalQuad.material.map = targetA.texture;
+  renderer.setRenderTarget(null);
+  renderer.render(finalScene, fboCamera);
+
+  let temp = targetA;
+  targetA = targetB;
+  targetB = temp;
+};
+
+onMounted(() => {
+  init();
+});
+</script>
+
+<template>
+  <canvas ref="brushRef"></canvas>
+</template>
+
+<style scoped></style>
--- shaders/about/SobelFragment.glsl
+++ shaders/about/SobelFragment.glsl
@@ -2,35 +2,48 @@ precision highp float;
 
 uniform sampler2D uTexture;
 uniform vec2 uResolution;
+uniform vec2 uMouse;
 uniform float uThreshold;
+uniform float uRadius;
 
 varying vec2 vUv;
+
+float circle(vec2 uv, vec2 pos, vec2 res, float r) {
+    uv -= pos;
+    uv.y *= res.y / res.x;
+    return 1.0 - smoothstep(r - (r * 0.01), r + (r * 0.01), dot(uv, uv) * 4.0);
+}
 
 float getLuminance(vec3 color) {
     return dot(color, vec3(0.2126, 0.7152, 0.0722));
 }
 
 void main() {
+    vec2 st = vUv;
     vec2 texelSize = 1.0 / uResolution;
 
-    float L_ul = getLuminance(texture2D(uTexture, vUv + vec2(-texelSize.x, texelSize.y)).rgb);
-    float L_up = getLuminance(texture2D(uTexture, vUv + vec2(0.0, texelSize.y)).rgb);
-    float L_ur = getLuminance(texture2D(uTexture, vUv + vec2(texelSize.x, texelSize.y)).rgb);
+    float L_ul = getLuminance(texture2D(uTexture, st + vec2(-texelSize.x, texelSize.y)).rgb);
+    float L_up = getLuminance(texture2D(uTexture, st + vec2(0.0, texelSize.y)).rgb);
+    float L_ur = getLuminance(texture2D(uTexture, st + vec2(texelSize.x, texelSize.y)).rgb);
 
-    float L_left = getLuminance(texture2D(uTexture, vUv + vec2(-texelSize.x, 0.0)).rgb);
-    float L_right = getLuminance(texture2D(uTexture, vUv + vec2(texelSize.x, 0.0)).rgb);
+    float L_left = getLuminance(texture2D(uTexture, st + vec2(-texelSize.x, 0.0)).rgb);
+    float L_right = getLuminance(texture2D(uTexture, st + vec2(texelSize.x, 0.0)).rgb);
 
-    float L_dl = getLuminance(texture2D(uTexture, vUv + vec2(-texelSize.x, -texelSize.y)).rgb);
-    float L_down = getLuminance(texture2D(uTexture, vUv + vec2(0.0, -texelSize.y)).rgb);
-    float L_dr = getLuminance(texture2D(uTexture, vUv + vec2(texelSize.x, -texelSize.y)).rgb);
+    float L_dl = getLuminance(texture2D(uTexture, st + vec2(-texelSize.x, -texelSize.y)).rgb);
+    float L_down = getLuminance(texture2D(uTexture, st + vec2(0.0, -texelSize.y)).rgb);
+    float L_dr = getLuminance(texture2D(uTexture, st + vec2(texelSize.x, -texelSize.y)).rgb);
 
     float Gx = (L_ul + 2.0 * L_left + L_dl) - (L_ur + 2.0 * L_right + L_dr);
     float Gy = (L_ul + 2.0 * L_up + L_ur) - (L_dl + 2.0 * L_down + L_dr);
 
     float edgeStrength = abs(Gx) + abs(Gy);
     float line = smoothstep(uThreshold - 0.05, uThreshold + 0.05, edgeStrength);
+    vec3 lineColor = vec3(1.0 - line);
 
-    vec3 finalColor = vec3(1.0 - line);
+    vec3 textureColor = texture2D(uTexture, st).rgb;
+
+    float ring = circle(st, uMouse, uResolution, uRadius);
+    vec3 finalColor = mix(textureColor, lineColor, ring);
 
     gl_FragColor = vec4(finalColor, 1.0);
 }
